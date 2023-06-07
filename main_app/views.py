0b76593162b07b37a08320f38a1cb51d6cd1db6a
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-06 21:57:59 -0700

    completes full crud functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Finch
 # Create your views here.
 
@@ -19,3 +20,21 @@ def finch_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, "main_app/finches/detail.html", {'finch': finch})
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'color', 'description', 'age']
+
+    # success_url = 'finches/{finch_id}'
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['color', 'description', 'age']
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
