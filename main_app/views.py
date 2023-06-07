db5c864bbc7540aba3214a54e2823e77a0212765
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-06 23:00:33 -0700

    completes 1:N relationship

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Finch
+from .forms import FeedingForm
 # Create your views here.
 
 
@@ -19,7 +20,19 @@ def finch_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, "main_app/finches/detail.html", {'finch': finch})
+    feeding_form = FeedingForm()
+    return render(request, "main_app/finches/detail.html", {'finch': finch, 'feeding_form': feeding_form})
+
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    # validate form
+    if form.is_valid():
+        # dont save form to db until it has finch_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
 
 
 class FinchCreate(CreateView):
@@ -31,7 +44,7 @@ class FinchCreate(CreateView):
 
 class FinchUpdate(UpdateView):
     model = Finch
-    # Let's disallow the renaming of a cat by excluding the name field!
+    # Let's disallow the renaming of a finch by excluding the name field!
     fields = ['color', 'description', 'age']
 
 
